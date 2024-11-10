197f86ec4f3af057cc973852c8822129a3017dcb
fshrmnsfrnd <nicon@o2mail.de>
2024-11-11 00:17:41 +0100

    zellen menge und prozent gestartet

--- Promillerechner/promillerechner.js
+++ Promillerechner/promillerechner.js
@@ -1,13 +1,14 @@
 //Elemente
 const buttonadddrink = document.getElementById("adddrink");
 var timetable = document.getElementById("timetable");
+const drinktypes = document.getElementsByClassName("typeofdrink");
 const buttonpromillenow = document.getElementById("promillejetzt");
 
 //globale Variablen
-var drinkcounter = 2;
+var drinkcounter = 1;
 
 //Konstanten
-const drinkpercent = new Map([
+const getranke = new Map([
     ["Bier", 5, 500],
     ["Wein", 12, 200],
     ["Sekt", 11, 100],
@@ -21,12 +22,28 @@ const drinkpercent = new Map([
     ["Champagner", 12, 100]
 ]);
 
+const timecell = `<input type="time" class="timeofdrink" id="time${drinkcounter}">`;
+const drinkcell = `<select class="typeofdrink" id="drink${drinkcounter}"><option value="bier">Bier</option><option value="wein">Wein</option></select>`;
+const percentcell = `<input type="number" class="percentofdrink" id="percent${drinkcounter}">`;
+const amountcell = `<input type="number" class="drinkamount" id="amount${drinkcounter}" value="${getranke.get(document.getElementById(`drink${drinkcounter}`))}">`;
+
 //Funktionen
 function adddrink() {
-    const newRow = timetable.insertRow();
-    newRow.insertCell(0).innerHTML = `<input type="time" id="time${drinkcounter}">`;
-    newRow.insertCell(1).innerHTML = `<select id="drink${drinkcounter}"><option value="bier">Bier</option><option value="wein">Wein</option></select>`;
+    var newRow = timetable.insertRow();
+    newRow.insertCell(0).innerHTML = timecell;
+    newRow.insertCell(1).innerHTML = drinkcell;
+    newRow.insertCell(2).innerHTML = percentcell;
+    newRow.insertCell(3).innerHTML = amountcell;
     drinkcounter++;
+}
+
+function updatedrinkvalues(){
+    for(var i = 1; i <= timetable.rows.length; i++){
+        var getrank = getranke.get(document.getElementById(`drink${i}`));
+        var [percent, amount] = getrank;
+        document.getElementById(`percent${i}`).innerHTML = percent;
+        document.getElementById(`amount${i}`).innerHTML = amount;
+    }
 }
 
 //Seitenstart
@@ -37,6 +54,10 @@ buttonadddrink.addEventListener("click", () => {
 });
 
 //Alkuholgehalt anzeigen fehlt noch
+
+drinktypes.addEventListener("change", () => {
+    updatedrinkvalues();
+});
 
 buttonpromillenow.addEventListener("click", () => {
 });
