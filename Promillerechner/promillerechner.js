7745061b67e493d52569c85fa5d46f7246414d10
fshrmnsfrnd <nicon@o2mail.de>
2024-11-10 23:17:40 +0100

    seitenstart hinzugefügt

--- Promillerechner/promillerechner.js
+++ Promillerechner/promillerechner.js
@@ -8,28 +8,35 @@ var drinkcounter = 2;
 
 //Konstanten
 const drinkpercent = new Map([
-    ["Bier", 5],
-    ["Wein", 12],
-    ["Sekt", 11],
-    ["Wodka", 40],
-    ["Whiskey", 40],
-    ["Rum", 38],
-    ["Gin", 37.5],
-    ["Tequila", 38],
-    ["Cognac", 40],
-    ["Likör", 20],
-    ["Champagner", 12]
+    ["Bier", 5, 500],
+    ["Wein", 12, 200],
+    ["Sekt", 11, 100],
+    ["Wodka", 40, 2],
+    ["Whiskey", 40, 2],
+    ["Rum", 38, 2],
+    ["Gin", 37.5, 2],
+    ["Tequila", 38, 2],
+    ["Cognac", 40, 2],
+    ["Likör", 20, 2],
+    ["Champagner", 12, 100]
 ]);
 
-buttonadddrink.addEventListener("click", () => {
+//Funktionen
+function adddrink() {
     const newRow = timetable.insertRow();
     newRow.insertCell(0).innerHTML = `<input type="time" id="time${drinkcounter}">`;
     newRow.insertCell(1).innerHTML = `<select id="drink${drinkcounter}"><option value="bier">Bier</option><option value="wein">Wein</option></select>`;
     drinkcounter++;
+}
+
+//Seitenstart
+adddrink();
+
+buttonadddrink.addEventListener("click", () => {
+    adddrink();
 });
 
 //Alkuholgehalt anzeigen fehlt noch
 
 buttonpromillenow.addEventListener("click", () => {
-   alcingramm = 
 });
