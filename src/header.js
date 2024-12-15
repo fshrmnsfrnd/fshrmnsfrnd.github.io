b84b33bfb2dc407a7f63c0afc479ada2a80cbd09
fshrmnsfrnd <nicon@o2mail.de>
2024-12-15 20:49:14 +0100

    header ready

--- src/header.js
+++ src/header.js
@@ -1,17 +1,12 @@
-class HeaderComponent extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = `
-      <nav class="headnav">
-                <a href="https://fshrmnsfrnd.github.io/">Home</a>
-                <a href="https://github.com/fshrmnsfrnd">Github</a>
-                <a href="https://fshrmnsfrnd.github.io/Luna/">Luna</a>
-                <a href="https://fshrmnsfrnd.github.io/Promillerechner/">Promillerechner</a>
-                <a href="https://fshrmnsfrnd.github.io/Snake/">Snake</a>
-                <!-- Neue Seitenlinks können hier hinzugefügt werden -->
-            </nav>
-    `;
-  }
-}
-customElements.define("header-component", HeaderComponent);
+document.addEventListener("DOMContentLoaded", () => {
+  const headerHTML = `
+    <a href="https://fshrmnsfrnd.github.io/"><h1>fshrmnsfrnd</h1></a>
+    <nav class="headnav">
+        <a href="https://fshrmnsfrnd.github.io/">Home</a>
+        <a href="https://github.com/fshrmnsfrnd">Github</a>
+        <a href="https://fshrmnsfrnd.github.io/Promillerechner/">Promillerechner</a>
+        <a href="https://fshrmnsfrnd.github.io/Snake/">Snake</a>
+    </nav>
+  `;
+  document.querySelector("header").innerHTML = headerHTML;
+});
